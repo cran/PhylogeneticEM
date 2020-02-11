33dbd5c3bbb1b37fd1e314764e8a8f3a2f5589d5
Paul Bastide <paul.bastide@m4x.org>
2020-02-11 08:40:05 +0000

    version 1.4.0

--- inst/doc/monkeys.R
+++ inst/doc/monkeys.R
@@ -6,7 +6,7 @@ plot(params_BM(p = 2),
      data = monkeys$dat, phylo = monkeys$phy,
      show.tip.label = TRUE, cex = 0.5, no.margin = TRUE)
 
-## ---- label="Fit_EM", warning=FALSE, message=FALSE-----------------------
+## ---- label="Fit_EM", warning=FALSE, message=FALSE----------------------------
 res <- PhyloEM(phylo = monkeys$phy,
                Y_data = monkeys$dat,
                process = "scOU",                   ## scalar OU model
@@ -18,23 +18,23 @@ res <- PhyloEM(phylo = monkeys$phy,
                Ncores = 2)                         ## parallel computations
 res
 
-## ---- fig.show='hold', fig.height=4, fig.width=7, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=7, warning=FALSE---------------
 plot(res)
 
-## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE---------------
 plot_criterion(res)
 
-## ---- fig.show='hold', fig.height=4, fig.width=7, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=7, warning=FALSE---------------
 plot(res, params = params_process(res, K = 5))
 
-## ---- echo=FALSE---------------------------------------------------------
+## ---- echo=FALSE--------------------------------------------------------------
 params_5 <- params_process(res, K = 5)
 
-## ---- fig.show='hold', fig.height=4, fig.width=8, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=8, warning=FALSE---------------
 plot(equivalent_shifts(monkeys$phy, params_process(res, K = 5)),
      show_shifts_values = FALSE, shifts_cex = 0.5)
 
-## ---- label="Fit_EM_rot", warning=FALSE, message=FALSE-------------------
+## ---- label="Fit_EM_rot", warning=FALSE, message=FALSE------------------------
 # An arbitrary rotation
 theta <- pi/4
 rot <- matrix(c(cos(theta), -sin(theta), sin(theta), cos(theta)),
@@ -55,28 +55,28 @@ res_rot <- PhyloEM(phylo = monkeys$phy,
                    parallel_alpha = TRUE,              
                    Ncores = 2)                         
 
-## ---- fig.show='hold', fig.height=4, fig.width=7, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=7, warning=FALSE---------------
 plot(res_rot)
 
-## ---- fig.show='hold', fig.height=3, fig.width=3, warning=FALSE----------
+## ---- fig.show='hold', fig.height=3, fig.width=3, warning=FALSE---------------
 plot(res, params = params_process(res, K = 3), no.margin = TRUE)
 plot(res_rot, params = params_process(res, K = 3), no.margin = TRUE)
 
-## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE---------------
 plot_criterion(res, "log_likelihood")
 plot_criterion(res_rot, "log_likelihood", pch = "+", add = TRUE)
 
-## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE---------------
 plot_criterion(res)
 plot_criterion(res_rot, pch = "+", add = TRUE)
 
-## ---- label="merge", warning=FALSE---------------------------------------
+## ---- label="merge", warning=FALSE--------------------------------------------
 res_merge <- merge_rotations(res, res_rot)
 
-## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE---------------
 plot_criterion(res_merge)
 
-## ---- label="Fit_EM_rot2", warning=FALSE, message = FALSE----------------
+## ---- label="Fit_EM_rot2", warning=FALSE, message = FALSE---------------------
 # An other rotation
 theta <- pi/3
 rot2 <- matrix(c(cos(theta), -sin(theta), sin(theta), cos(theta)),
@@ -100,10 +100,10 @@ res_rot2 <- PhyloEM(phylo = monkeys$phy,
 # Merge
 res_merge2 <- merge_rotations(res, res_rot, res_rot2)
 
-## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE---------------
 plot_criterion(res_merge2)
 
-## ---- label="Fit_EM_negative", warning=FALSE, message = FALSE------------
+## ---- label="Fit_EM_negative", warning=FALSE, message = FALSE-----------------
 res_neg <- PhyloEM(phylo = monkeys$phy,
                    Y_data = monkeys$dat,
                    process = "scOU",
@@ -114,9 +114,9 @@ res_neg <- PhyloEM(phylo = monkeys$phy,
                    nbr_alpha = 4,            ## 2 negative, 2 positive (should be more)
                    allow_negative = TRUE)    ## allow negative alpha in the grid
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 params_process(res_neg, K = 0)
 
-## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE----------
+## ---- fig.show='hold', fig.height=4, fig.width=4, warning=FALSE---------------
 plot_criterion(res_neg, "BGHmlraw")
 
